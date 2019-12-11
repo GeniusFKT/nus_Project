4b8f42d0d5c5aa937bb55ae7986fe51f8a4ecb20
Kenneth <yancheng1112@sjtu.edu.cn>
2019-12-11 16:44:43 +0800

    Update model

--- train.py
+++ train.py
@@ -1,36 +1,6 @@
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, LSTM, RepeatVector, TimeDistributed
-from datautil import data_loader
-
-n_steps_in, n_steps_out = 5, 1
-n_features_in, n_features_out = 27, 1
-batch_size = 20
-
+from util import modelutil
 
 if __name__ == "__main__":
-    loader = data_loader.data_loader("D:\\VSCode_Project\\Python\\Project\\data\\level1.csv", n_steps_in, n_steps_out, 0.8)
-    trainX, trainY, testX, testY = loader.get_data()
-
-    # define model [Vanilla LSTM]
-    model = Sequential()
-    model.add(LSTM(200, activation='relu', input_shape=(n_steps_in, n_features_in)))
-    model.add(RepeatVector(n_steps_out))
-    model.add(LSTM(200, activation='relu', return_sequences=True))
-    model.add(TimeDistributed(Dense(n_features_out)))
-
-    loss = tf.keras.losses.MeanSquaredError(name="Loss")
-
-    metrics = [tf.keras.metrics.MeanAbsolutePercentageError(name="mean_absolute_percentage"),
-               tf.keras.metrics.MeanSquaredError(name="mean_square")]
-
-    model.compile(optimizer='adam', loss=loss, metrics=metrics)
-
-    model.summary()
-
-    model.fit(trainX, trainY, batch_size=batch_size, epochs=5, validation_data=(testX, testY), shuffle=True)
-    score = model.evaluate(testX, testY, batch_size=batch_size)
-    print('Test score:', score)
-
-    pre = model.predict(testX)
+    model = modelutil.model_seq2seq()
+    model.train()
 
---
+++ util/modelutil.py
@@ -0,0 +1,60 @@
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense, LSTM, RepeatVector, TimeDistributed
+from util import data_loader
+from seq2seq.models import Seq2Seq
+from keras.callbacks import ModelCheckpoint
+
+n_steps_in, n_steps_out = 5, 1
+n_features_in, n_features_out = 27, 1
+batch_size = 20
+EPOCHS = 20
+
+class my_model():
+    def __init__(self):
+        self.loader = data_loader.data_loader("D:\\VSCode_Project\\Python\\Project\\data\\level1.csv", n_steps_in, n_steps_out, 0.8)
+        trainX, trainY, testX, testY = self.loader.get_data()
+        self.trainX = trainX
+        self.trainY = trainY
+        self.testX = testX
+        self.testY = testY
+
+class model_origin(my_model):
+    def train(self):
+        model = Sequential()
+        model.add(LSTM(200, activation='relu', input_shape=(n_steps_in, n_features_in)))
+        model.add(LSTM(200, activation='relu'))
+        model.add(RepeatVector(n_steps_out))
+        model.add(LSTM(200, activation='relu', return_sequences=True))
+        model.add(TimeDistributed(Dense(n_features_out)))
+
+        loss = tf.keras.losses.MeanSquaredError(name="Loss")
+
+        metrics = [tf.keras.metrics.MeanAbsolutePercentageError(name="mean_absolute_percentage"),
+                   tf.keras.metrics.MeanSquaredError(name="mean_square")]
+
+        checkpoint = ModelCheckpoint("model_save", monitor="mean_absolute_percentage", verbose=1, save_best_only=True, mode='min')
+
+        model.compile(optimizer='adam', loss=loss, metrics=metrics)
+
+        model.summary()
+
+        model.fit(self.trainX, self.trainY, batch_size=batch_size, callbacks=[checkpoint], epochs=EPOCHS, validation_data=(self.testX, self.testY), shuffle=True)
+
+
+class model_seq2seq(my_model):
+    def train(self):
+        model = Seq2Seq(batch_input_shape=(batch_size, n_steps_in, n_features_in), hidden_dim=200, output_length=n_steps_out, output_dim=n_features_out, depth=3)
+
+        loss = tf.keras.losses.MeanSquaredError(name="Loss")
+
+        metrics = [tf.keras.metrics.MeanAbsolutePercentageError(name="mean_absolute_percentage"),
+                   tf.keras.metrics.MeanSquaredError(name="mean_square")]
+
+        checkpoint = ModelCheckpoint("model_save", monitor="mean_absolute_percentage", verbose=1, save_best_only=True, mode='min')
+
+        model.compile(optimizer='adam', loss=loss, metrics=metrics)
+
+        model.summary()
+
+        model.fit(self.trainX, self.trainY, batch_size=batch_size, callbacks=[checkpoint], epochs=EPOCHS, validation_data=(self.testX, self.testY), shuffle=True)
